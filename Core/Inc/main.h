adc2b17c7daef147be7c8212e2744b682e75a6d2
Utyf <utyff@mail.ru>
2026-02-27 03:08:10 +0300

    ll update

--- Core/Inc/main.h
+++ Core/Inc/main.h
@@ -40,6 +40,7 @@ extern "C" {
 #include "stm32h7xx_ll_tim.h"
 #include "stm32h7xx_ll_usart.h"
 #include "stm32h7xx_ll_gpio.h"
+#include "stm32h7xx_ll_hsem.h"
 
 #if defined(USE_FULL_ASSERT)
 #include "stm32_assert.h"
